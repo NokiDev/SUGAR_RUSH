6f8523e8bea12ba5a0526d8d2dce1975e62e9abf
Léo <leo.massonnat@hotmail.fr>
2019-06-06 12:46:44 +0000

    Add prefab for Metro Map

---
+++ UnityProject/Assets/Scripts/MetroMap/Station.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Station : MonoBehaviour, IPointerClickHandler
+{
+    public string name;
+    public GameObject image;
+    public List<GameObject> lines;
+
+    public bool isHere = false;
+    public bool isActive = false;
+    public bool isSelect = false;
+    private float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+
+        if (isHere)
+        {
+            gameObject.GetComponent<CanvasRenderer>().SetColor(new Color(0.4f, 0.4f, 0.9f, 1f));
+        }
+        else if (isSelect)
+        {
+            gameObject.GetComponent<CanvasRenderer>().SetColor(new Color(0.4f, 0.9f, 0.4f, 1f));
+        }
+        else
+        {
+            float opacity = isActive ? ((Mathf.Cos(timer * 6) + 1) / 4) + 0.5f : 0.3f;
+            gameObject.GetComponent<CanvasRenderer>().SetColor(new Color(1f, 1f, 1f, opacity));
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (isActive && !isHere)
+        {
+            Debug.Log("CLICK" + eventData);
+            foreach (GameObject station in GameObject.FindGameObjectsWithTag("station"))
+            {
+                station.GetComponent<Station>().isSelect = false;
+            }
+            isSelect = true;
+        }
+    }
+}
