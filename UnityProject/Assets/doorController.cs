2d7bc5a9bb5c5d20833cad01f7ff290337d96dc3
Léo <leo.massonnat@hotmail.fr>
2019-06-14 11:53:11 +0000

    add unlock door script

---
+++ UnityProject/Assets/doorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    public int keysRequired = 2;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<pocket>().useKeys(keysRequired))
+            {
+                Destroy(gameObject);
+
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/pocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pocket : MonoBehaviour
+{
+    private int keys = 0;
+
+    public void addKey()
+    {
+        keys++;
+    }
+
+    public int getKeys()
+    {
+        return keys;
+    }
+
+    public bool useKeys(int nbUsed)
+    {
+        if (keys < nbUsed)
+        {
+            return false;
+        }
+        else
+        {
+            keys -= nbUsed;
+            return true;
+        }
+    }
+
+}
