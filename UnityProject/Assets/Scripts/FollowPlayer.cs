b57b973672086b599f5be11d2ad18b8b1180cc57
Matthias <matthias.reimund>
2019-06-14 11:23:02 +0200

    NPC Random Move + Policeman
    NPC move in a Random way and PoliceMan follow you when you are in a zone

---
+++ UnityProject/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Player;
+    public float Speed = 1;
+    public float StepBeforeCameraMove;
+    private float LimiteXUp;
+    private float LimiteXDown;
+    private float LimiteYUp;
+    private float LimiteYDown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LimiteXUp = Player.position.x + StepBeforeCameraMove;
+        LimiteXDown = Player.position.x - StepBeforeCameraMove;
+
+        LimiteYUp = Player.position.y + StepBeforeCameraMove;
+        LimiteYDown = Player.position.y - StepBeforeCameraMove;
+        bool WillMove = transform.position.x < LimiteXUp && transform.position.x > LimiteXDown && transform.position.y < LimiteYUp && transform.position.y > LimiteYDown;
+
+        if (WillMove)
+        {
+            transform.position = Vector3.Lerp(transform.position, Player.position, Speed);
+            transform.position += new Vector3(0, 0, -10);
+        }
+        
+        
+    }
+}
---
+++ UnityProject/Assets/Scripts/VisionCone.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionCone : MonoBehaviour
+{
+    public bool isSeen = false;
+    public Transform targetTransform;
+    public Transform OwnTransform;
+    public Vector3 StartPosition;
+    public Vector3 direction;
+    public float Angle;
+
+    private float offset = 90f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rb2d = GetComponent<Rigidbody2D>();
+        //Vector3 transformRight = transform.position + new Vector3(0.12f,0);
+
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        OwnTransform = GetComponent<Rigidbody2D>().transform;
+
+
+        if (isSeen)
+        {
+            
+            Vector2 direction = targetTransform.position - new Vector3(transform.position.x, transform.position.y);
+
+            direction.Normalize();
+            var KeepedAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Angle = 180 + KeepedAngle;
+            transform.rotation = Quaternion.Euler(Vector3.forward * (Angle + offset));
+        }
+
+      
+
+
+
+
+    }
+
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isSeen = true;
+        targetTransform = collision.GetComponent<Rigidbody2D>().transform;
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isSeen = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isSeen = false;
+        transform.position = StartPosition;
+    }
+
+}
+
+
+
+
+//RaycastHit2D hit = Physics2D.Raycast(transform.position, collision.transform.position, lenght, ContactFilter2D.layerMask);
+//if (hit.collider != null)
+//{
+//    distance = hit.distance;
+//}
