571e2290823b1c4d8b3326605e633f775e524161
NokiDev <noki.dev@gmail.com>
2019-06-14 16:02:35 +0200

    added player detection with raycast and player layer :

--- UnityProject/Assets/PathManager.cs
+++ UnityProject/Assets/PathManager.cs
@@ -5,38 +5,62 @@ using Pathfinding;
 
 
 // type de trajet : aleatoire, ronde, ligne ...
-
-
-
 public class PathManager : MonoBehaviour
 {
     public List<Transform> LocationsLists = new List<Transform>();
     private AIDestinationSetter AIDestinationSetter;
-    private int LocationStep = 0;
-
+    public int LocationStep = 0;
+    private bool stop = false;
+    public PlayerDetector detector;
+    
     void Start()
-    {
+    { 
+        detector.onPlayerDetected += FollowPlayer;
+        detector.onPlayerLost += UnFollowPlayer;
         AIDestinationSetter = GetComponent<AIDestinationSetter>();
         AIDestinationSetter.target = LocationsLists[0];
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "LocationHelper") {
+        if (!stop && collision.tag == "LocationHelper") {
+
+            if(LocationsLists.Count > 0) {
 
             LocationStep++;
 
-            if (LocationStep == LocationsLists.Count) {
+            if (LocationStep >= LocationsLists.Count) {
                 LocationStep = 0;
             }
 
             AIDestinationSetter.target = LocationsLists[LocationStep];
+
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void FollowPlayer(GameObject player)
+    {
+        stop = true;
+        GetComponent<AIPath>().speed = 5;
+        GetComponent<SpriteRenderer>().color = Color.red;
+        AIDestinationSetter.target = player.transform;
         
     }
+
+    void UnFollowPlayer(GameObject player)
+    {
+        GetComponent<AIPath>().speed = 2;
+        GetComponent<SpriteRenderer>().color = Color.yellow;
+        stop = false;
+        AIDestinationSetter.target = LocationsLists[LocationStep];
+    }
+
+
 }
---
+++ UnityProject/Assets/PlayerDetector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public float playerDetectionDistance = 5f;
+    public float distanceFromStartPosition = 50f;
+    public LayerMask layerMask;
+
+    public delegate void PlayerDetectedCB(GameObject player);
+    public PlayerDetectedCB onPlayerDetected;
+    public PlayerDetectedCB onPlayerLost;
+
+    public bool permanent;
+    private GameObject player = null;
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!permanent && player != null)
+        {
+            if (Vector3.Distance(gameObject.transform.position, startPosition) >= distanceFromStartPosition)
+            {
+                player = null;
+                onPlayerLost(null);
+            }
+        }   
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            var direction = collision.gameObject.transform.position - gameObject.transform.position;
+            
+             RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, direction, playerDetectionDistance, layerMask.value);
+            if (hit.collider != null)
+            {
+                if(hit.collider.gameObject.tag == "Player")
+                {
+                    player = hit.collider.gameObject;
+                    onPlayerDetected?.Invoke(player);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+    }
+
+
+
+}
