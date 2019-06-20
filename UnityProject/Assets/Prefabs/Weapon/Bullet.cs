01c9bef0ae14e2788fd3191d744e852e5a02f8b4
ShrimpLab <jules.rognoni@gmail.com>
2019-06-20 23:17:42 +0200

    add weapon

---
+++ UnityProject/Assets/Prefabs/Weapon/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int collision_counter_limit = 0;
+    private int collision_counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (collision_counter >= collision_counter_limit){
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision_counter++;
+    }
+}
---
+++ UnityProject/Assets/Prefabs/Weapon/Control.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    private Transform transform;
+    public GameObject bullet;
+
+    public float bullet_speed;
+
+    private Vector3 mouse_pos;
+    private Vector3 weapon_pos;
+    private Vector3 weapon_vect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        weapon_pos = gameObject.transform.position;
+        weapon_vect = mouse_pos - weapon_pos;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Shot();
+        
+    }
+
+    void Shot()
+    {
+
+        var shot_vector = (Vector2)(weapon_vect);
+        shot_vector = shot_vector.normalized * bullet_speed;
+
+        var bullet_start_position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0);
+        var b = Instantiate(bullet, bullet_start_position, Quaternion.identity, gameObject.transform.parent.parent);
+
+        b.GetComponent<Rigidbody2D>().velocity = (shot_vector);
+    }
+}
